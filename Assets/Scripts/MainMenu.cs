cd7a359c151b351c0b2b684aaf4288c461b9ccfd
Steven Meiklejohn <stevenmeiklejohn700@gmail.com>
2019-10-01 14:35:34 +0100

    Initial commit contains main menu scene with start button.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Application.targetFrameRate = 60;
+	}
+
+	public void GoToGame(){
+		SceneManager.LoadScene("Game");
+	}
+}
